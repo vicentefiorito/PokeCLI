a09e358869bbb49c4b3e670ef0e46c28199e7745
vicentefiorito <vicentefiorito@gmail.com>
2024-04-25 23:35:52 -0400

    managed to get the raw json from the api calls

--- command_catch.go
+++ command_catch.go
@@ -1,5 +1,25 @@
 package main
 
-func commandCatch(cfg *config, args ...string) {
-	
+import (
+	"errors"
+	"fmt"
+)
+
+func commandCatch(cfg *config, args ...string) error {
+	// if the user doesn't input the pokemon name
+	if len(args) != 1 {
+		return errors.New("please include a pokemon name")
+	}
+
+	// gets the pokemon name from args
+	pokemonName := args[0]
+
+	// gets the data from the API call
+	pokemon, err := cfg.pokeapiClient.GetPokemon(pokemonName)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(pokemon)
+	return nil
 }
